885e61756f91601001da2fd86095fcf3509eac69
dingga <dingga270499@gmail.com>
2020-11-02 00:09:12 +0700

    membuat model dan seeder

---
+++ database/seeds/RoleSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+// ! import model
+use Spatie\Permission\Models\Role;
+
+
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //? membuat role user
+        Role::create([
+            'name' => 'admin',
+            'guard_name' => 'web'
+        ]);
+
+
+        //? membuat role user
+        Role::create([
+            'name' => 'user',
+            'guard_name' => 'web'
+        ]);
+    }
+}
---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,37 @@
+<?php
+// ! import model user
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //? membuat column user
+        $admin = User::create([
+            'name' => 'admin role',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('123456')
+
+        ]);
+
+        //! tambahkan fungsi Asign role
+        $admin->assignRole('admin');
+
+        //? membuat column user
+        $user = User::create([
+            'name' => 'user role',
+            'email' => 'user@gmail.com',
+            'password' => bcrypt('123456')
+
+        ]);
+
+        //! tambahkan fungsi Asign role
+        $user->assignRole('user');
+    }
+}
