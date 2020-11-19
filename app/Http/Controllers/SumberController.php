f39c81b2a7f9fc86580a39bbdf8af22060be78ab
suka paus <182410102006@students.unej.ac.id>
2020-11-19 16:21:57 +0700

    form add pemasukan

--- app/Http/Controllers/SumberController.php
+++ app/Http/Controllers/SumberController.php
@@ -2,38 +2,44 @@
 
 namespace App\Http\Controllers;
 
+
+use Webpatser\Uuid\Uuid;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
 
 class SumberController extends Controller
 {
 
-    public function index(){
-        $sumber =  \DB::table('tb_pemasukan')->get();
+    public function index()
+    {
+        $sumber =  DB::table('tb_pemasukan')->get();
 
 
         return view('sumber.sumber_index', compact('sumber'));
     }
 
-    public function add(){
+    public function add()
+    {
 
         return view('sumber.sumber_add');
     }
 
-    public function store(Request $request){
-        $this -> validate($request,[
-            'nama'=>'required'
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'nama' => 'required'
         ]);
 
-        $nama = $request -> nama;
-        \DB::table('tb_pemasukan')->insert([
-            'id' => \Uuid::generate(4),
+        $nama = $request->nama;
+        DB::table('tb_pemasukan')->insert([
+            'id' => Uuid::generate(4),
             'nama' => $nama,
-            'created_at' =>date('Y-m-d H:i:s'),
-            'updated_at' =>date('Y-m-d H:i:s')
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
 
         ]);
 
         return redirect('sumber-pemasukan');
-
     }
 }
