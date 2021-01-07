49eb01c32a3c165070f16ef89d9598b1b9e7c65e
suka paus <182410102006@students.unej.ac.id>
2021-01-07 22:26:21 +0700

    🐛 fixing url

--- resources/views/pertemuan/pertemuan_index.blade.php
+++ resources/views/pertemuan/pertemuan_index.blade.php
@@ -4,7 +4,7 @@
     <div class="col-lg-6 col-7">
       <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md">
         <ol class="breadcrumb breadcrumb-links breadcrumb-dark">
-          <li class="breadcrumb-item"><a href="/home"><i class="fas fa-home"></i></a></li>
+          <li class="breadcrumb-item"><a href="{{ url('/home') }}"><i class="fas fa-home"></i></a></li>
           <li class="breadcrumb-item active" aria-current="page">data pertemuan</li>
         </ol>
       </nav>
