22053eb975fe78e30ce7edea7b8a2b6b1c6b77fb
suka paus <182410102006@students.unej.ac.id>
2021-01-07 23:58:03 +0700

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,7 +32,6 @@
 
             <div class="card-body px-lg-5 py-lg-5">
               <div class="text-center text-muted mb-4">
-                <small>selamat datang di management keuangan</small>
                 <small>admin  username:admin@gmail.com</small>
                 <small>admin  password:123456</small>
                 <small>user  username:user@gmail.com</small>
